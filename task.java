928c28302db523bb714279f40e4a24e927b402e0
Cavad-007 <cavadagayev07@gmail.com>
2026-03-14 23:12:44 +0400

    Update task.java

--- task.java
+++ task.java
@@ -222,6 +222,125 @@
 
                 //String, String builder research length,charAt,substring, split
 
+
+
+                    public class task {
+            public static void main(String[] args) {
+                //0,1,2,3........
+//        int a = 30;//0
+//        int b = 20;//1
+//        int c = 10;//2
+
+                //{30,20,10}
+                //Size is fixed
+//        int[] numbers = new int[100];
+//        numbers[0] = a;
+//        numbers[1] = b;
+//        numbers[2] = c;
+
+//        System.out.println(numbers[0]+" "+numbers[1]+" "+numbers[2]);
+
+//        int[] numbers2 = {30, 20, 10};
+
+
+//        System.out.println(numbers2[0]+" "+numbers2[1]+" "+numbers2[2]);
+
+//        numbers2[3]=40;
+//
+//        System.out.println(numbers2[3]);
+
+//        numbers2[2] = 50;
+//        System.out.println(numbers2[2]);
+                //iteration
+                //x
+//        for(int i =0;i<numbers2.length;i++){
+//            numbers2[i]+=10;
+//        }
+//
+//        for(int i =0;i<numbers2.length;i++){
+//
+//          System.out.println(numbers2[i]);        }
+//    }
+
+//        for(int number: numbers2) {
+//            System.out.println(number);
+//
+//
+//        }
+
+
+                //2D arrays
+                //{1,2,3}
+                //{2,34,12}
+                //{123,321,4521}
+
+//        int[][] matrix = {
+//                {1,2,3},
+//                {4,5,6},
+//                {7,8,9}
+//        };
+//
+//        System.out.println(matrix[0][1]);
+//
+//        for(int i = 0; i < matrix.length; i++){
+//            for(int j = 0; j < matrix[i].length; j++){
+//                System.out.print(matrix[i][j] + " ");
+//            }
+//        }
+
+                //String, String builder research length,charAt,substring, split
+
+//               Siyahinin max - ni ve boyukduru(>) kicikdirile(<) evez etdikde min - u tapir
+//
+//        int[] array = {12, 14 , 6 , -9 , 0};
+//        int a = array[0] ;
+//        for (int i = 0; i < array.length; i++){
+//            int a1 = array[i];
+//            if (a1 > a){
+//                a = a1;
+//            }
+//        }
+//                System.out.println(a);
+
+
+//
+//        max ve min funksiya ile
+//        int[] a = {23 , 0 , 6 , 9 ,20};
+//        int min = Arrays.stream(a).min().getAsInt();
+//                System.out.println(min);
+
+//
+//         Stringi tersine cevirme
+//
+//        String b = "Salam";
+//        String a = " ";
+//        for(int i = b.length() - 1; i >= 0 ; i= i -1){
+//            a = a + b.charAt(i);
+//
+//        }
+//        System.out.println(a);
+
+
+//        String a = "ana";
+//                String b = "";
+//        for (int i = a.length()-1 ; i>=0 ; i = i -1){
+//          b = b + a.charAt(i);
+//        }
+//        if (a.equals(b)){
+//            System.out.println("polidromdur");
+//        }
+//        else{
+//            System.out.println("Polidrom deyil");
+//        }
+//                System.out.println(b);
+//
+//
+//                String text = "Java Python C++";
+//
+//                String[] arr = text.split(" ");
+//                System.out.println(text);
+//
+
             }
         }
 
